4832f871f481107ee77343c29fd7a628d898a8c8
Jung JaeWoong <jjw_0609@naver.com>
2023-08-13 22:57:43 +0900

    23.08.13 update

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-projects/'
-    : '/',
-    outputDir: 'docs'
+    base: '/vue-project/'
 })
